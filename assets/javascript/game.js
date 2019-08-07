f4d607ac42870a87c27da4114dd5e85d25e32b26
Wesley Berry <wesleyberry52@gmail.com>
2019-08-06 22:41:33 -0500

    fixed some spelling innacuracies

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,13 @@
 var rotate = 360;
 $(".intro a[href^='#']").on("click", function(e) {
-
-    // prevent default anchor click behavior
     e.preventDefault();
- 
-    // store hash
+
     var hash = this.hash;
  
-    // animate
     $('html, body').animate({
         scrollTop: $(hash).offset().top
     }, 1000, function(){
  
-    // when done, add hash to url
-    // (default click behaviour)
     window.location.hash = hash;
     });
 });
